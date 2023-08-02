474e0e14d16561abbdb20d0406c3f8d5c82e3613
Arpit Tyagi <arpittyagi102@gmail.com>
2023-08-02 15:30:42 +0530

    Uploaded my code

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const socketEvents = require('./startup/socketEvents.js')
+const server = http.createServer(app);
+
+if (process.env.NODE_ENV === 'test') {
+  require("dotenv").config({ path: ".env.test" });
+} else {
+  require("dotenv").config();
+}
+
+app.use(bodyParser.json());
+
+require('./startup/cors.js')(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://getlinkup.vercel.app"],
+    methods: ["GET", "POST"],
+  },
+});
+socketEvents(io);
+
+require('./startup/db')(app);
+
+const PORT = process.env.PORT || 3001;
+
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = server;
---
+++ startup/socketEvents.js
@@ -0,0 +1,44 @@
+const { on } = require("nodemon");
+
+module.exports = (io) => {
+  let onlinePeople = [];
+  const emailToSocketIdMap = new Map();
+
+  io.on('connection', (socket) => {
+    let userData;
+
+    socket.on("initialData", (userDatacame) => {
+      userData = userDatacame;
+      if (!userData) return;
+      if (!onlinePeople.includes(userData.email)) {
+        onlinePeople.push(userData.email);
+        console.log(userData.name + " is online");
+        console.log(emailToSocketIdMap);
+      }
+      emailToSocketIdMap.set(userData.email, socket.id);
+      io.emit("online-people", onlinePeople);
+    });
+
+    socket.on("disconnect", () => {
+      if (userData) {
+        if (onlinePeople.includes(userData.email)) {
+          console.log(userData.name + " is offline");
+          const temp = [...onlinePeople];
+          temp.splice(userData.email, 1);
+          onlinePeople = temp;
+          emailToSocketIdMap.delete(userData.email);
+        }
+        io.emit("online-people", onlinePeople);
+      }
+    });
+
+    socket.on("send-message", (messageData) => {
+      const recipientEmail = messageData.sendto.email;
+      const recipientSocketID = emailToSocketIdMap.get(recipientEmail);
+      //console.log(messageData);
+      socket.emit("recieve-message", messageData);
+      io.to(recipientSocketID).emit("recieve-message", messageData);
+    });
+
+  });
+};
